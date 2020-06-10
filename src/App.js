75ce649690ece6da4971353f06f52eb25ab5248b
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-06-10 09:26:56 -0500

    Day two

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { ListOfCategories } from "./components/ListOfCategories";
-import { GlobalStyle } from "./GlobalStyles";
+import { GlobalStyle } from "./styles/GlobalStyles";
 import { ListOfPhotoCards } from "./components/ListOfPhotoCards";
+import { Logo } from "./components/Logo";
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </div>
---
+++ src/styles/animation.js
@@ -0,0 +1,19 @@
+import { keyframes, css } from "styled-components";
+
+const fadeInKeyframes = keyframes`
+  from {
+    filter: blur(5px);
+    opacity: 0;
+  }
+
+  to {
+    filter: blur(0);
+    opacity: 1;
+  }
+
+`;
+
+export const fadeIn = ({ time = "1sg", type = "ease" } = {}) =>
+  css`
+    animation: ${time} ${fadeInKeyframes} $type;
+  `;
